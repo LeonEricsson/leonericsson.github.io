10b672b4d732bf3c234d7b7e3f08113ed93dca11
Leon Ericsson <leon.ericsson@icloud.com>
2024-01-09 15:19:56 +0100

    update about

--- components/AboutCard/index.js
+++ components/AboutCard/index.js
@@ -34,7 +34,7 @@ function AboutCard() {
           <ul className="list-none ml-4 pt-2">
             <li className="before:content-['-'] before:pr-2">ggerganov's <a href="https://github.com/ggerganov/llama.cpp" style={{ color: 'blue' }}>llama.cpp</a> (<a href="https://github.com/ggerganov/llama.cpp/pull/4484" style={{ color: 'blue' }}>#4484</a>)</li>
             <li className="before:content-['-'] before:pr-2"><a href="https://github.com/ml-explore/mlx" style={{ color: 'blue' }}>mlx</a> is an array framework for apple silicon (<a href="https://github.com/ml-explore/mlx-examples/pull/202" style={{ color: 'blue' }}>#202</a> <a href="https://github.com/ml-explore/mlx-examples/pull/237" style={{ color: 'blue' }}>#237</a> <a href="https://github.com/ml-explore/mlx-examples/pull/19" style={{ color: 'blue' }}>#19</a>) </li>
-            <li className="before:content-['-'] before:pr-2"> <a href="https://github.com/LeonEricsson/Tensor-Puzzles" style={{ color: 'blue' }}>tensor puzzles</a> from sasha rush</li>
+            <li className="before:content-['-'] before:pr-2"> <a href="https://github.com/LeonEricsson/Tensor-Puzzles" style={{ color: 'blue' }}>tensor puzzles</a> and <a href="https://github.com/LeonEricsson/GPU-Puzzles" style={{ color: 'blue' }}>cuda puzzles</a> from sasha rush</li>
             <li className="before:content-['-'] before:pr-2"> community driven <a href="https://github.com/ohmyzsh/ohmyzsh" style={{ color: 'blue' }}>ohmyzsh</a></li>
             <li className="before:content-['-'] before:pr-2"> <a href="https://github.com/google/jax" style={{ color: 'blue' }}>jax</a>: the lovechild of autograd and xla</li>
           </ul> 
